b247e155d908a826a4acc89daa86f73f05ae3539
r0b456 <47432625+r0b456@users.noreply.github.com>
2019-04-25 15:37:55 -0400

    Add files via upload

---
+++ miproyecto/app/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> secretopi.com</title><!--titulo-->
+    <meta http-equiv="Content-Type" content="ie=edge"; charset="UTF-8"> <!--sirve para escribir caracteres especiales en la web -->
+
+    <div class="barra_superior">
+	<link rel="stylesheet" href="../css/barra_css.css">
+	<link rel = "shortcut icon" type = "image" href = "../imagenes/tituloprin.jpg">    
+
+	
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    
+     <!--menu-->
+     
+     <link rel="stylesheet" href="../css/menucss.css">
+     
+     
+     
+     <script src="../js/menu.js"></script>
+
+</head>
+
+
+<body>
+<header>
+		<div class="menu_bar">
+			<a href="#" class="bt-menu"><span class="icon-list2"></span>Menu</a>
+		</div>
+ 
+		<nav>
+			<ul>
+				<li><a href="#"><span class="icon-house"></span>Inicio</a></li>
+				<li><a href="#"><span class="icon-suitcase"></span>Categorias</a></li>
+				<li><a href="#"><span class="icon-rocket"></span>recientes</a></li>
+				<li><a href="#"><span class="icon-earth"></span>Mas Votados</a></li>
+				<li><a href="#"><span class="icon-mail"></span>Publicar historia</a></li>
+			</ul>
+		</nav>
+	</header>
+</body>
+<footer>
+
+</footer>
+</html>
